7abdfeff49131da9cd03a3e90f8f26632d37dd2d
Rafiulomar <personal.rafiulomar@gmail.com>
2024-05-12 20:36:55 +0600

    blogs fake data added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import Blogs from "./components/Blogs/Blogs"
+import Header from "./components/Header/Header"
+
 function App() {
 
   return (
     <>
-      <h1 className="text-6xl">
-        Hello world!
-      </h1>
+      <Header></Header>
+      <Blogs></Blogs>
     </>
   )
 }
